28e43b624db944b1bcae7d8ac9147f09c20cf89d
Nick Ward <n.ward13@yahoo.com>
2019-02-19 19:07:08 -0700

    break active account into separate component

---
+++ client/src/js/components/ActiveAccount.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Blockie from './Blockie';
+import getWeb3 from '../utils/getWeb3';
+
+class ActiveAccount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ethBalance: null,
+      contractBalance: [],
+    }
+  }
+
+  componentWillMount = async () => {
+    
+    // get the eth balance for the active address 
+    let ethBalance = this.props.web3.utils.fromWei(
+      await this.props.web3.eth.getBalance(this.props.account),
+      'ether'
+    );
+
+    // get the balance of the active address in the payments contract
+    let contractBalance = this.props.web3.utils.fromWei(
+      await this.props.contract.methods.balances(this.props.account).call(),
+      'ether'
+    );
+
+    // set the state with the new balances
+    this.setState({ethBalance: ethBalance, contractBalance: contractBalance});
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="line">
+          <Blockie address={this.props.account} size={3} />
+          <h4 className="address">
+            <strong>Address: </strong> 
+            {this.props.account}
+          </h4>
+        </div>
+        <div className="balances">
+          <h5>
+            <strong>ETH balance: </strong>
+            {this.state.ethBalance}
+          </h5>
+          <h5>
+            <strong>Contract balance: </strong>
+            {this.state.contractBalance}
+          </h5>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ActiveAccount;
